5bfe0a725bb5819bdb7df4e01e16930060f1ef39
obscuredc <austinberm@gmail.com>
2022-05-01 20:37:23 -0400

    engine development

--- hosting/asset/meta/gamestate.js
+++ hosting/asset/meta/gamestate.js
@@ -4,3 +4,9 @@ export let states = {
     acceptMovement: false,  //Send movement requests (WASD Space Etc) to the playermanager
     acceptMouseEvents: true //send mousemove/mouseclick events to handlers like GuiHandler
 }
+
+export function logable() {
+    return `gamestate ---\nisPaused${states.isPaused == true ? "✅" : "❌"}\ncurrentGUI "${states.currentGUI}"\nacceptMovement${states.acceptMovement == true ? "✅" : "❌"}\nacceptMouseEvents${states.acceptMouseEvents == true ? "✅" : "❌"}`
+}
+import { utils } from "../../engine/utils.js"
+utils.post("✅meta/gamestate")
